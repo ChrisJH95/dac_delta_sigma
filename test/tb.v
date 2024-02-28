13b7f349193847d1bf5a8c077e1f5249153a476b
ChrisJH95 <huemerc95@gmail.com>
2024-02-28 21:44:13 +0100

    verilog files added

--- test/tb.v
+++ test/tb.v
@@ -1,10 +1,10 @@
 `default_nettype none `timescale 1ns / 1ps
-
+//`include "tt_um_delta_sigma.v"
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
 module tb ();
-
+   parameter BW = 16;
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
@@ -23,7 +23,16 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  wire [15:0] ui16_in;
+  wire uo1_out;
+  wire rst_in;
+  assign rst_in = ~rst_n;
+  assign ui16_in = {ui_in, uio_in};
+  assign uo1_out = uo_out[0];
+  
+  tt_um_delta_sigma 
+  #( BW )
+  dut(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -31,14 +40,20 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      //.ui_in  (ui_in),    // Dedicated inputs
+      //.uo_out (uo_out),   // Dedicated outputs
+      //.uio_in (uio_in),   // IOs: Input path
+      .dac_i(ui16_in),
+      .dac_o(uo1_out),  // IOs: Output path
+      //.uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      //.ena    (ena),      // enable - goes high when design is selected
+      .clk_i  (clk),      // clock
+      .rst_i  (rst_in)    // not reset
   );
 
+ //input clk_i , // clock
+ //input rst_i , // reset
+ //input signed [BW-1:0] dac_i, //input
+ //output wire dac_o
+ 
 endmodule
